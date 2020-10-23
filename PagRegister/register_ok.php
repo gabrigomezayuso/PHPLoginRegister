89c5169e88c926adc534bb18bee424b65b45b0c3
gabrigomezayuso <72407504+gabrigomezayuso@users.noreply.github.com>
2020-10-23 21:01:48 +0200

    0.0.1
    Login i Register Funcional

---
+++ PagRegister/register_ok.php
@@ -0,0 +1,75 @@
+<!doctype html>
+<html lang="es">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Registrar</title>
+    <!-- jQuery + Bootstrap JS -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+<!-- sdfdsfsdsd -->
+    <div class="App"> 
+        <div class="vertical-center">
+            <div class="inner-block">
+                
+		<?php
+
+				header("Content-Type: text/html;charset=utf-8");
+
+					//Parámetros que vienen del POST
+					$firstname=$_POST["firstname"];
+					$lastname=$_POST["lastname"];
+					$email=$_POST["email"];
+					$mobilenumber=$_POST["mobilenumber"];
+					$password=$_POST["password"];
+
+
+					//Parámetros de conexión
+					$servidor="localhost";
+					$usuario="root";
+					$contraseña="usbw";
+					$bd="test";
+
+
+				//realizamos la conexión
+				$con=mysqli_connect($servidor,$usuario,$contraseña,$bd);
+				if(!$con)
+				{
+					die("Con se ha podido realizar la conexión: ". mysqli_connect_error() . "<br>");
+				}
+				else
+				{
+					mysqli_set_charset($con,"utf8");
+					// echo "Felicidades lerdo! te has conectado a la BD <br>";
+					$_SESSION["con"]=$con;
+				}
+
+				$consulta=mysqli_query($con,"insert into usuarios values 
+				('$firstname','$lastname','$email','$mobilenumber','$password')");
+
+				if(!$consulta)
+				{
+					die("Error en la consulta");
+				}
+				else
+				{
+						echo "<strong>¡Felicidades!</strong>, te has regsitrado con éxito.";
+				}
+
+
+
+
+		?>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
